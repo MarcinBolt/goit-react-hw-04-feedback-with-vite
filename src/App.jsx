0aa30bb40ee5559303df58eb3eb3f23239252efe
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-06-11 12:19:22 +0200

    fix App; delete unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
 import { FeedbackOptions } from './components/FeedbackOptions/FeedbackOptions';
 import { Section } from './components/Section/Section';
 import { Statistics } from './components/Statistics/Statistics';
-import { useFeedback } from './components/feedbackContext/feedbackContext';
 
 function App() {
-  const {
-    good,
-    neutral,
-    bad,
-    countTotalFeedback,
-    countPositiveFeedbackPercentage,
-  } = useFeedback();
-
   return (
     <>
       <Section title="Please leave feedback">
         <FeedbackOptions />
       </Section>
       <Section title="Statistics">
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={countTotalFeedback()}
-          positive={countPositiveFeedbackPercentage()}
-        />
+        <Statistics />
       </Section>
     </>
   );
